45685bec3d2f4a11ec39f9c9c4ef216f37fefde0
TTShubham <shubham.sharma@timbletech.com>
2020-09-22 15:18:26 +0530

    Initial commit

---
+++ app/src/main/java/com/timble/glance/mylottie/MainActivity.java
@@ -0,0 +1,74 @@
+package com.timble.glance.mylottie;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class MainActivity extends Activity {
+
+    LottieAnimationView thumb_up,verified;
+    LottieAnimationView thumb_down;
+    LottieAnimationView toggle;
+    int flag = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        thumb_up = findViewById(R.id.lav_thumbUp);
+        verified = findViewById(R.id.verified);
+
+        verified.setProgress(0);
+        verified.pauseAnimation();
+        verified.playAnimation();
+
+        thumb_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                thumb_down.setProgress(0);
+                thumb_down.pauseAnimation();
+                thumb_up.playAnimation();
+                Toast.makeText(MainActivity.this, "Cheers!!", Toast.LENGTH_SHORT).show();
+                //---- Your code here------
+            }
+        });
+
+        thumb_down = findViewById(R.id.lav_thumbDown);
+        thumb_down.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                thumb_up.setProgress(0);
+                thumb_up.pauseAnimation();
+                thumb_down.playAnimation();
+                Toast.makeText(MainActivity.this, "Boo!!", Toast.LENGTH_SHORT).show();
+                //---- Your code here------
+            }
+        });
+
+        toggle = findViewById(R.id.lav_toggle);
+        toggle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeState();
+            }
+        });
+    }
+
+    private void changeState() {
+        if (flag == 0) {
+            toggle.setMinAndMaxProgress(0f, 0.43f); //Here, calculation is done on the basis of start and stop frame divided by the total number of frames
+            toggle.playAnimation();
+            flag = 1;
+            //---- Your code here------
+        } else {
+            toggle.setMinAndMaxProgress(0.5f, 1f);
+            toggle.playAnimation();
+            flag = 0;
+            //---- Your code here------
+        }
+    }
+}
